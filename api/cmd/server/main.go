977435d3d86be285dbf2bd0d617d0df34d4f90d8
Brenden Bissett <brenden.bissett@gmail.com>
2025-11-25 13:37:56 +1300

    Added gitignore

---
+++ api/cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to Help-Me-Budget API")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
